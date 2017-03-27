301606e0fe8ad09018505a501e5983fa3f7ec74b
Marcos Dourado <marcosdourado92@gmail.com>
2017-03-27 00:42:15 -0300

    creating constant for difficult victories

--- grails-app/controllers/nfl/bet/ResultController.groovy
+++ grails-app/controllers/nfl/bet/ResultController.groovy
@@ -6,19 +6,20 @@ import grails.converters.*
 
 class ResultController {
 	static responseFormats = ['json', 'xml']
+	public static final int VD = 1
 
-    def index() {
+	def index() {
 			def bets = Bet.list()
 			bets.each { bet ->
 				if (bet.game.winner.id == bet.beatingTeam.id) {
 					if (bet.game.scoreDiference > 10) {
-						if (bet.type == 1) {
+						if (bet.type == VD) {
 							bet.points = 10
 						} else {
 							bet.points = 5
 						}
 					} else {
-						if (bet.type == 1) {
+						if (bet.type == VD) {
 							bet.points = 5
 						} else {
 							bet.points = 10
